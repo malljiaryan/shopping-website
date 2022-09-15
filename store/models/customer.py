f287963164fe1219e93bb6d7fa73c218a0128e2d
Aryan Pratap Mall <aryanpratap.ig@gmail.com>
2022-09-15 23:32:05 +0530

    first commit

---
+++ store/models/customer.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=54)
+    last_name = models.CharField (max_length=60)
+    phone = models.CharField(max_length=16)
+    email=models.EmailField()
+    password = models.CharField(max_length=100)
+
+    #to save the data
+    def register(self):
+        self.save()
+
+
+    @staticmethod
+    def get_customer_by_email(email):
+        try:
+            return Customer.objects.get(email= email)
+        except:
+            return False
+
+
+    def isExists(self):
+        if Customer.objects.filter(email = self.email):
+            return True
+
+        return False
